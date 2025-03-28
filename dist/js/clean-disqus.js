2a0fa3407110e157cbef65c0bed9877b197e351e
Aleksandr Filatov <greybax@gmail.com>
2025-03-28 02:07:28 +0000

    fix

--- dist/js/clean-disqus.js
+++ dist/js/clean-disqus.js
@@ -7,10 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
         // Check if the element has a `src` attribute and if it does not contain "https://disqus.com"
         if (element.src && !element.src.includes('https://disqus.com')) {
           element.remove(); // Remove the element
-          console.log(`Removed element with ID: ${element.id} and src: ${element.src}`);
+          //console.log(`Removed element with ID: ${element.id} and src: ${element.src}`);
         }
       });
   
-      console.log(`Processed ${elements.length} elements.`);
+      //console.log(`Processed ${elements.length} elements.`);
     }, 2000); // Delay execution by 2000 milliseconds (2 seconds)
   });
